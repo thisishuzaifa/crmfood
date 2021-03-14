e41f29144f7fa37f19f08d4e783b704b16f40e89
Huzaifa Khalid <71489575+thisishuzaifa@users.noreply.github.com>
2021-03-13 23:47:30 -0800

    Defined models and relations

---
+++ customers/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+
+class User(AbstractUser):
+    pass
+
+
+class Customer(models.Model):
+
+    MENU_CHOICES = ( 
+        ('Breakfast','Breakfast'),
+        ('Lunch', 'Lunch'),
+        ('Dinner', 'Dinner'),
+    )
+
+    first_name = models.CharField(max_length = 30)
+    last_name = models.CharField(max_length = 30)
+    phone_number = models.IntegerField(default = 0)
+    billed = models.BooleanField(default = False)
+    meal = models.CharField(choices = MENU_CHOICES, max_length = 100)
+    special_files = models.FileField(blank = True, null = True)
+    agent = models.ForeignKey("Agent", on_delete=models.SET_NULL, null = True)
+
+class Agent(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
